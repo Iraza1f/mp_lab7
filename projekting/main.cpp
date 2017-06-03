163dbc470d7935844200b41bd5916f272cec60b2
Eugene <no>
2017-06-03 07:40:27 +0700

    singleton

---
+++ projekting/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+class Singleton
+{
+    private:
+        static Singleton* instance;
+        Singleton();
+
+    public:
+        static Singleton* getInstance();
+};
+
+Singleton* Singleton::instance = 0;
+
+Singleton* Singleton::getInstance()
+{
+    if (instance == 0)
+    {
+        instance = new Singleton();
+    }
+
+    return instance;
+}
+
+
+Singleton::Singleton()
+{}
+
+int main()
+{
+    Singleton* s = Singleton::getInstance();
+    Singleton* r = Singleton::getInstance();
+    cout << s << endl;
+    cout << r << endl;
+
+    return 0;
+}
